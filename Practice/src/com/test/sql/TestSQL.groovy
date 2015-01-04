205dedd4f2bc8ea7a1c1bd33272d7aab79be2a55
rameshsunkari <rameshsunkari@gmail.com>
2015-01-04 21:37:02 +0530

    converting sql output to xml

--- Practice/src/com/test/sql/TestSQL.groovy
+++ Practice/src/com/test/sql/TestSQL.groovy
@@ -1,17 +1,30 @@
 package com.test.sql
 
 import groovy.sql.Sql
+import groovy.xml.MarkupBuilder
 
 class TestSQL {
 
 	static main(args) {
-		def sql = Sql.newInstance("jdbc:mysql://localhost:3306/groovytest", "testuser",
-			"testuser", "com.mysql.jdbc.Driver")
-		
-		def food = sql.dataSet('Person')
-		def cheeses = food.findAll { it.last_name == 'test%' }
-		cheeses.each { println "Person ${it}" }
-	
-	}
+		def schema = 'groovytest'
+		def sql = Sql.newInstance("jdbc:mysql://localhost:3306/$schema", "testuser",
+				"testuser", "com.mysql.jdbc.Driver")
 
+		/* Request */
+		def req = """
+				SELECT id,first_name,last_name FROM ${schema}.person
+		"""
+		def out = new File('/home/user/Ramesh/Learning/Grails/Sample/xmlOutput.xml')
+		def writer = new FileWriter( out )
+		def xml = new MarkupBuilder( writer )
+
+		xml.agents {
+			sql.eachRow( req as String  ) { /* For each row output detail */ row ->
+				xml.agent(id:row.id) {
+					firstname( row.first_name )
+					lastname( row.last_name )
+				}
+			}
+		}
+	}
 }
